772ecb2fdc222c8ed56ddcd5454d29142fff0ecc
YugenOk <omerkorat@gmail.com>
2023-12-01 10:42:34 +0200

    Update process_th_db.py

--- process_th_db.py
+++ process_th_db.py
@@ -1,5 +1,12 @@
 """
-This script demonstrates the handling of a thematic database as created by
+This script demonstrates the handling of a thematic database as created by create_th_db.py.
+
+The json dataset is first parsed so that we have a list of documents each mapped to an event which is a dictionary from thematic roles to their content, and from 'predicate' to the event's predicate (usually the main verb).
+
+We then collect (i) all the possible contents of the most important thematic roles (we call them NPs, or noun phrases), because this is what they usually are, and (ii) all predicates, and embed all of them using some embedding model. In this example we are using finbert, because we are using financial data for the example, but you can use any embedding model which suits your data.
+
+
+
 
 """
 
@@ -24,6 +31,30 @@ def change_outliers_to_inds(labels):
             labels[i] = index
             index += 1
     return labels
+
+def extract_relevant_sentence(doc, event_dict):
+    """
+    Give a document and an event dictionary,
+    return the sentence in the document which is most likely to be the one represented by the event dict.
+    """
+    doc = doc.replace('U.S.', 'US')
+    # Tokenize the paragraph into sentences
+    sentences = doc.split('. ')
+
+    # Extract key terms from the event dictionary
+    key_terms = set()
+    for value in event_dict.values():
+        key_terms.update(value.split())
+
+    # Score each sentence based on the number of key terms it contains
+    sentence_scores = []
+    for sentence in sentences:
+        score = sum(word in sentence for word in key_terms)
+        sentence_scores.append((score, sentence))
+
+    # Select the sentence with the highest score
+    most_relevant_sentence = max(sentence_scores, key=lambda x: x[0])[1]
+    return most_relevant_sentence
 
 def calculate_probabilities(vectors):
     """
@@ -89,7 +120,6 @@ def embs_from_docs_pipeline(docs,embedding_model):
     Picks the last vector in the model's output, because this is the top layer.
     If the document is too long, iteratively truncate it by a 100 characters until it fits.
     Return a matrix of all document embedding vectors.
-
     """
     print('Creating document embeddings...')
     embs = []
@@ -112,7 +142,13 @@ def embed_finbert(docs):
     pl = pipeline('feature-extraction', 'yiyanghkust/finbert-tone', device='cuda:0')
     return embs_from_docs_pipeline(docs,pl)
 
-embed = embed_finbert # replace with your favorite embedding function
+embed = embed_finbert # replace with your favorite embedding function (it should take an iterable of strings and return a matrix)
+
+# This is tangential to our overall point in this example,
+# but we need to filter out legal statements so we've built a classifier that does that.
+from pickle import load
+with open('output/is_business_rf_classifier.pkl','rb') as f:
+    is_business_classifier = load(f)
 
 # Load any other saved database that was generated using the thematic prompt
 ID = '64d11d67b8a4a9b9e363ceac'
@@ -121,6 +157,14 @@ source2db = json.load(open(f'output/{dataset_name}-them.json','r'))
 
 docs = list(source2db.keys())
 db = list(source2db.values())
+
+N = len(docs)
+
+embs = embed(docs)
+business_labels = is_business_classifier.predict(embs)
+
+docs = [docs[i] for i in range(N) if business_labels[i]]
+db = [db[i] for i in range(N) if  business_labels[i]]
 
 # First evaluate all entries in the database from json to python
 evaled_db = []
@@ -154,7 +198,8 @@ get_conts = lambda l: [x[0] for x in l]
 NPs = (get_conts(role2cont_and_pos['Agent'])\
       + get_conts(role2cont_and_pos['Patient'])\
       + get_conts(role2cont_and_pos['Goal'])\
-      + get_conts(role2cont_and_pos['Recipient']))
+      + get_conts(role2cont_and_pos['Recipient'])\
+      + + get_conts(role2cont_and_pos['Instrument']))
 
 NPs = list(set([x for x in NPs if type(x) is str]))
 nembs = embed(NPs)
@@ -171,18 +216,18 @@ pembs = embed(pred_types)
 from sklearn.cluster import KMeans
 
 # NP labels
-labels = KMeans(n_clusters=40).fit_predict(nembs)
+np_labels = KMeans(n_clusters=40).fit_predict(nembs)
 
 # Map each NP to its clusters (both as a dict and a dataframe)
-NP2clust = {np:label for np,label in zip(NPs,labels)}
-something = {i:[np,label] for i,(np,label) in enumerate(zip(NPs,labels))}
+NP2clust = {np:label for np,label in zip(NPs, np_labels)}
+something = {i:[np,label] for i,(np,label) in enumerate(zip(NPs, np_labels))}
 np_clust_df = pd.DataFrame.from_dict(something, orient='index',
                                      columns=['np','cluster'])
 
 # For each NP cluster, pick the shortest one in the cluster as a stand-in
 def get_shortest(lst):
     return min(lst, key=lambda x: len(x))
-clust2np = {label:get_shortest(np_clust_df[np_clust_df.cluster==label].values[:,0]) for label in labels}
+clust2np = {label:get_shortest(np_clust_df[np_clust_df.cluster==label].values[:,0]) for label in np_labels}
 
 
 # Display examples from each cluster
@@ -192,13 +237,13 @@ def print_cluster_exs(c, s=10):
     for i in sampl:
         print(NPs[i])
 
-for i in range(len(set(labels))-1):
+for i in range(len(set(np_labels)) - 1):
     print(i)
     print_cluster_exs(i)
     print('-----------------')
 
 # Cluster the predicates
-pred_labels = KMeans(n_clusters=400).fit_predict(pembs)
+pred_labels = KMeans(n_clusters=int(len(pembs)/4)).fit_predict(pembs)
 
 # Create a dictionary and a dataframe mapping for predicates as well
 pred_labels = change_outliers_to_inds(pred_labels)
@@ -207,10 +252,13 @@ something = {i:[pred,label] for i,(pred,label) in enumerate(zip(pred_types,pred_
 pred_clust_df = pd.DataFrame.from_dict(something, orient='index',columns=['pred','cluster'])
 clust2pred = {label:pred_clust_df[pred_clust_df.cluster==label].iloc[0].pred for label in pred_labels}
 
+# -1 stands for "unclustered" in many clustering packages
+# we just convert it to a string
 clust2np[-1] = '-1'
 clust2pred[-1] = '-1'
 
-def reconstruct_event_dict(event):
+# We define it here because it uses the clusters we just found
+def clustered_event_dict(event):
     """Based on the found clusters, this function returns a clustered representation
     of an event to a readable one, where each cluster index is replaced by
     a representative string (shortest member of the cluster)."""
@@ -224,14 +272,23 @@ def reconstruct_event_dict(event):
 
 # Collapse each NP into its cluster ID to compress the dataset
 collapsed_db = []
+non_collapsed_db = []
 for entry in evaled_db:
     new_events = []
     for event in entry:
         new_event = {}
+
+        # Iterate over the events
         for  role, content in event.items():
+            # Change every pred or NP to their cluster ID
             if role == 'Predicate':
                 new_event[role] = pred2clust.get(content,-1)
             else:
+                # The reason we have some cases here is because ChatGPT is not 100% consistent in its output
+                # so I had to just manually account for all cases.
+                # If you run into errors in this code it is likely because there of further inconsistencies in the output,
+                # and unfortunately there isn't an elegant solution. But this should work in most cases:
+
                 ignore = False
                 if type(content) is dict:
                     content = list(content.keys())[0]
@@ -240,7 +297,7 @@ for entry in evaled_db:
                     if all([type(x) is str for x in content]):
                         content = ' and '.join(content)
                     elif all([type(x) is dict for x in content]):
-                        # this case is negligible, ignore for now
+                        # this case is negligible, ignore
                         ignore = True
                 if not ignore:
                     new_event[role] = NP2clust.get(content, -1)
@@ -256,31 +313,43 @@ for i,events in enumerate(collapsed_db):
         ind2doc[ind]=i
         ind+=1
 
-
 # This is a flat list of all events, not dividided by docs
 events = flatten(collapsed_db)
 
-# Small event represetnation, just as a tuple of clusters
+# This is just for reference as we want to display the uncollapsed events for comprehensibility
+raw_db = flatten(evaled_db)
+
+# Small event represetnation, just as a tuple of clusters.
+# You can use this as sentence embeddings, for various purpose as we do below.
 small_events = [(event.get('Predicate',None),event.get('Agent',None),event.get('Patient',None),event.get('Goal',None),event.get('Recipient',None)) for event in events]
 
+# Example usage:
 # Calculate coocurrence probabilities based on the small event representations as vectors
 # See function docstring for explanation
 vectors = small_events
 position_prob, pair_prob, vector_probs = calculate_probabilities(vectors)
 
-
-# Example usage:
-# Display low probability (therefore surprising, interesting) events
-
 # Find events with extreme k probabilities
 k = 10
 lowest_k_indices, highest_k_indices = find_top_k_extremes_indices(vector_probs, k)
 
-# Print the full doc from which the event is taken followed by the reconstructed event
-# You have to figure out which sentence in the doc corresponds to the event,
-# but it should be pretty straightforward.
+# Display events with low probabilities, which identifies surprising/interesting events in our data
+# Print the full doc from which the event is taken followed by the reconstructed event.
+# The way the prompt is setup now, ChatGPT doesn't tell us which event corresponds to which sentence,
+# so you have to figure it out from the event representation. But this should be pretty straightforward.
 for i in lowest_k_indices:
 
+    print('Document':)
     print(docs[ind2doc[i]])
-    print(reconstruct_event_dict(events[i]))
+    print()
+    print('Sentence:')
+    relevant_sentence = extract_relevant_sentence(paragraph, event_dict)
+    print()
+    print('Event:')
+    print(raw_db[i])
+    print()
+    print('Event with clusters:')
+    print(clustered_event_dict(events[i]))
     print('------------------')
+
+
