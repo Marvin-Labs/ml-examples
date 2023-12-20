89a27a3ce102ddf13f3be471463267177dd179d4
YugenOk <omerkorat@gmail.com>
2023-12-20 16:40:45 +0200

    Add files via upload

--- create_th_db.py
+++ create_th_db.py
@@ -36,24 +36,22 @@ This represents the document:
 
 To query ChatGPT using the load balancer object as we do here, you would need OpenAI and Azure API key, as well as an Azure API Base.
 """
-import json
 
 import lbgpt
 import diskcache
 import pandas as pd
-import os
-
 import jinja2
 import asyncio
-from datasets import load_dataset
+import json
 
 OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY'
 AZURE_OPENAI_API_KEY = 'YOUR_AZURE_OPENAI_API_KEY'
 AZURE_OPENAI_API_BASE = 'YOUR_AZURE_OPENAI_API_BASE'
 
+
 # A jinja2 environment to render the prompt
 temp_env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader('prompts'),
+    loader=jinja2.FileSystemLoader('../prompts'),
     autoescape=jinja2.select_autoescape(),
 )
 
@@ -63,13 +61,12 @@ chatgpt = lbgpt.LoadBalancedGPT(
     openai_api_key=OPENAI_API_KEY,
     azure_api_key=AZURE_OPENAI_API_KEY,
     azure_api_base=AZURE_OPENAI_API_BASE,
-    cache=cache,
     azure_model_map={
-        "gpt-3.5-turbo-0613": "gpt-35-turbo-0613",
-        "gpt-3.5-turbo": "gpt-35-turbo-0613",
-        "gpt-4": "gpt-4-0613",
-        "gpt-4-0613": "gpt-4-0613",
-    },
+    "gpt-3.5-turbo-0613": "gpt-35-turbo-0613",
+    "gpt-3.5-turbo": "gpt-35-turbo-0613",
+    "gpt-4": "gpt-4-0613",
+    "gpt-4-0613": "gpt-4-0613",
+},
 )
 
 
@@ -82,12 +79,13 @@ def thematize(docs):
     # Create a list of queries, one for each doc
     comp_list = []
     for doc in docs:
+
         d = {
             # Use the jinja2 environment to render the prompt
             # both from the user side (the argument, which is doc),
             # and the system side, which is the instruction to chatgpt.
-            'messages': [
-                {'role': 'system',
+            'messages' : [
+                {'role':'system',
                  'content':
                      temp_env.get_template('system_thematic.jinja2').render()},
                 {'role': 'user',
@@ -102,20 +100,19 @@ def thematize(docs):
         comp_list.append(d)
 
     # Perform the querrying using the LoadBalancedGPT object
+    cache = diskcache.Cache('cache')
     outs = asyncio.run(chatgpt.chat_completion_list(comp_list))
     return [out.choices[0].message['content'] for out in outs]
 
-
 # Load the documents you would like to use
 # Here we use 100 documents from the AG News dataset as an example
-dataset_name = 'ag_news'
-dataset = load_dataset(dataset_name, split="train")
+from datasets import load_dataset
+dataset = load_dataset("ag_news", split="train")
 docs = dataset.shuffle().select(range(100))['text']
 
 # Create the database
 db = thematize(docs)
 
 # Save database
-os.makedirs('output', exist_ok=True)
-source2db = {c: r for c, r in zip(docs, db)}
-json.dump(source2db, open(f'output/{dataset_name}.json', 'w'))
+source2db = {c:r for c,r in zip(docs, db)}
+json.dump(source2db, open(f'data/ag-news-100-them.json','w'))
--- process_th_db.py
+++ process_th_db.py
@@ -1,29 +1,26 @@
 """
 This script demonstrates the handling of a thematic database as created by create_th_db.py.
 
-The json dataset is first parsed so that we have a dictionary mapping a document to a list of event representations of the sentences in that document. An event is a dictionary from thematic roles (one of which is a predicate, or the main verb) to the strings which assume that role in the event.
+The json dataset is first parsed so that we have a list of documents each mapped to an event which is a dictionary from thematic roles to their content, and from 'predicate' to the event's predicate (usually the main verb).
 
-We then collect (i) all the possible contents of the most important thematic roles (we call them NPs, or noun phrases),
-because this is what they usually are, and (ii) all predicates, and embed all of them using some embedding model.
+We then collect (i) all the possible contents of the most important thematic roles (we call them NPs, or noun phrases), because this is what they usually are, and (ii) all predicates, and embed all of them using some embedding model.
 
-In this example we are using distilbert-base-uncased, which is a simple yet effective embedding model, but you can
-use any model which suits your application.
+In this example we are using finbert, because we are using financial data for the example, but you can use any embedding model which suits your data.
 
 The contents are then clustered, and event likelihood is calculated based on role/cluster coocurrence frequencies.
 """
 
-import json
-from collections import defaultdict
 
-import numpy as np
-import pandas as pd
+
+from collections import defaultdict
 import torch
-from sklearn.cluster import KMeans
 from tqdm import tqdm
 from transformers import pipeline
+import json
+import numpy as np
+import pandas as pd
 
 flatten = lambda l: [item for sublist in l for item in sublist]
-
 
 def change_outliers_to_inds(labels):
     """Changes all the -1s to their own cluster, indexed by order."""
@@ -34,7 +31,6 @@ def change_outliers_to_inds(labels):
             labels[i] = index
             index += 1
     return labels
-
 
 def extract_relevant_sentence(doc, event_dict):
     """
@@ -60,7 +56,6 @@ def extract_relevant_sentence(doc, event_dict):
     most_relevant_sentence = max(sentence_scores, key=lambda x: x[0])[1]
     return most_relevant_sentence
 
-
 def calculate_probabilities(vectors):
     """
     This means probabilities are calculated over the probability of clusters in their thematic roles.
@@ -78,18 +73,18 @@ def calculate_probabilities(vectors):
     for vector in vectors:
         for i, value in enumerate(vector):
             position_freq[i][value] += 1
-            for j in range(i + 1, len(vector)):
+            for j in range(i+1, len(vector)):
                 pair_freq[(i, j)][(value, vector[j])] += 1
 
     # Calculate probabilities
-    position_prob = {pos: {val: count / total_vectors for val, count in values.items()}
+    position_prob = {pos: {val: count/total_vectors for val, count in values.items()}
                      for pos, values in position_freq.items()}
-    pair_prob = {(pos1, pos2): {val_pair: count / total_vectors for val_pair, count in pairs.items()}
+    pair_prob = {(pos1, pos2): {val_pair: count/total_vectors for val_pair, count in pairs.items()}
                  for (pos1, pos2), pairs in pair_freq.items()}
 
     # Marginalize None by setting its probability to 1
     for pos in position_prob.keys():
-        position_prob[pos][None] = 1
+        position_prob[pos][None]=1
 
     # Assign probabilities to each vector by multiplying the probability of all of its components
     vector_probabilities = []
@@ -97,7 +92,7 @@ def calculate_probabilities(vectors):
         prob = 1
         for i, value in enumerate(vector):
             prob *= position_prob[i].get(value, 0)
-            for j in range(i + 1, len(vector)):
+            for j in range(i+1, len(vector)):
                 pair = (value, vector[j])
                 prob *= pair_prob[(i, j)].get(pair, 0)
         vector_probabilities.append(prob)
@@ -119,8 +114,7 @@ def find_top_k_extremes_indices(vector_probs, k):
 
     return lowest_k_indices, highest_k_indices
 
-
-def embs_from_docs_pipeline(docs, embedding_model):
+def embs_from_docs_pipeline(docs,embedding_model):
     """
     Use embedding_model to embed each document in docs.
     Picks the last vector in the model's output, because this is the top layer.
@@ -131,101 +125,104 @@ def embs_from_docs_pipeline(docs, embedding_model):
     embs = []
     truncs = []
     for doc in tqdm(docs):
-        trunc = 3000  # Initial truncation
+        trunc = 3000 # Initial truncation
         while True:
             try:
                 # Truncate document before entering the model, and pick the last output layer ([0,-1])
-                embs.append(embedding_model(doc[:trunc], return_tensors='pt')[0, -1])
+                embs.append(embedding_model(doc[:trunc],return_tensors='pt')[0, -1])
                 truncs.append(trunc)
                 break
             except RuntimeError:
                 # If it doesn't fit, truncate by a little more
-                trunc -= 100
+                trunc-=100
     return torch.stack(embs)
 
-
-def embed_finbert(docs):
+def embed_hypernym(docs):
     """Use the embedding function with the finbert model."""
-    pl = pipeline('feature-extraction', 'distilbert-base-uncased', device='cuda:0')
-    return embs_from_docs_pipeline(docs, pl)
+    pl = pipeline('feature-extraction', 'sohomghosh/LIPI_FinSim3_Hypernym', device='cuda:0')
+    return embs_from_docs_pipeline(docs,pl)
+
+embed = embed_hypernym # replace with your favorite embedding function (it should take an iterable of strings and return a matrix)
 
 
-embed = embed_finbert  # replace with your favorite embedding function (it should take an iterable of strings and return a matrix)
-
-# Load any saved database that was generated using the thematic prompt
-# here we use the one we created in create_th_db.py
-dataset_name = 'ag_news'
-source2db = json.load(open(f'output/{dataset_name}-them.json', 'r'))
+source2db = json.load(open(f'data/ag-news-100-them.json','r'))
 
 docs = list(source2db.keys())
 db = list(source2db.values())
 
 N = len(docs)
 
-embs = embed(docs)
-
 # First evaluate all entries in the database from json to python
 evaled_db = []
 for i, doc_rep in enumerate(db):
+    # If for any reason there is a formatting issue that causes the evaluation to fail,
+    # just drop it. It's not worth worrying about.
     try:
         doc_rep = eval(doc_rep)
     except:
         doc_rep = []
+    # If the doc is only one event long it would be represetend as a dict
+    # rather than a list of dicts. So we create a 1-element list out of it
+    # to fit the format.
     if type(doc_rep) == dict:
         doc_rep = [doc_rep]
 
     evaled_db.append(doc_rep)
 
+# This is just for reference as we want to display the uncollapsed events for comprehensibility
+raw_db = flatten(evaled_db)
+raw_db_df = pd.DataFrame(raw_db)
+
 # Collect metadata from each role to all of its possible values
 # This dict maps each role in the database to a list of its values (content)
 # alongside indices (i,j) representing where this role had this value within the db.
 role2cont_and_pos = defaultdict(list)
-for i, doc_rep in enumerate(evaled_db):
-    for j, event in enumerate(doc_rep):
-        for role, content in event.items():
+for i,doc_rep in enumerate(evaled_db):
+    for j,event in enumerate(doc_rep):
+        for role,content in event.items():
             # collapse duplicate roles (like "Agent2", the second agent in the event)
             if role[-1] in '23456789':
                 role = role[:-1]
-            role2cont_and_pos[role].append((content, (i, j)))
+            role2cont_and_pos[role].append((content,(i,j)))
 
 # To get all contents for a certain role
 get_conts = lambda l: [x[0] for x in l]
 
+
 # Get the content of all important NPs and embed them
-NPs = (get_conts(role2cont_and_pos['Agent']) \
-       + get_conts(role2cont_and_pos['Patient']) \
-       + get_conts(role2cont_and_pos['Goal']) \
-       + get_conts(role2cont_and_pos['Recipient']) \
-       + + get_conts(role2cont_and_pos['Instrument']))
+NPs = (get_conts(role2cont_and_pos['Agent'])\
+      + get_conts(role2cont_and_pos['Patient'])\
+      + get_conts(role2cont_and_pos['Goal'])\
+      + get_conts(role2cont_and_pos['Recipient'])\
+      + get_conts(role2cont_and_pos['Instrument']))
 
 NPs = list(set([x for x in NPs if type(x) is str]))
 nembs = embed(NPs)
 
 # Same for predicates
-preds = get_conts(role2cont_and_pos['Predicate'])
+preds =  get_conts(role2cont_and_pos['Predicate'])
 pred_types = list(set(preds))
 pembs = embed(pred_types)
+
 
 # Now we run clustering with several hyparam choices to compare them and choose the best one
 # KMeans is one of the simplest ways to cluster for demonstration purposes,
 # but the optimal cllustering scheme changes massively depending on the application.
+from sklearn.cluster import KMeans
 
 # NP labels
 np_labels = KMeans(n_clusters=40).fit_predict(nembs)
 
 # Map each NP to its clusters (both as a dict and a dataframe)
-NP2clust = {np: label for np, label in zip(NPs, np_labels)}
-something = {i: [np, label] for i, (np, label) in enumerate(zip(NPs, np_labels))}
+NP2clust = {np:label for np,label in zip(NPs, np_labels)}
+something = {i:[np,label] for i,(np,label) in enumerate(zip(NPs, np_labels))}
 np_clust_df = pd.DataFrame.from_dict(something, orient='index',
-                                     columns=['np', 'cluster'])
-
+                                     columns=['np','cluster'])
 
 # For each NP cluster, pick the shortest one in the cluster as a stand-in
 def get_shortest(lst):
     return min(lst, key=lambda x: len(x))
-
-
-clust2np = {label: get_shortest(np_clust_df[np_clust_df.cluster == label].values[:, 0]) for label in np_labels}
+clust2np = {label:get_shortest(np_clust_df[np_clust_df.cluster==label].values[:,0]) for label in np_labels}
 
 
 # Display examples from each cluster
@@ -235,29 +232,28 @@ def print_cluster_exs(c, s=10):
     for i in sampl:
         print(NPs[i])
 
-
 for i in range(len(set(np_labels)) - 1):
     print(i)
     print_cluster_exs(i)
     print('-----------------')
 
 # Cluster the predicates
-pred_labels = KMeans(n_clusters=int(len(pembs) / 4)).fit_predict(pembs)
+pred_labels = KMeans(n_clusters=int(len(pembs)/4)).fit_predict(pembs)
 
 # Create a dictionary and a dataframe mapping for predicates as well
 pred_labels = change_outliers_to_inds(pred_labels)
-pred2clust = {pred: label for pred, label in zip(pred_types, pred_labels)}
-something = {i: [pred, label] for i, (pred, label) in enumerate(zip(pred_types, pred_labels))}
-pred_clust_df = pd.DataFrame.from_dict(something, orient='index', columns=['pred', 'cluster'])
-clust2pred = {label: pred_clust_df[pred_clust_df.cluster == label].iloc[0].pred for label in pred_labels}
+pred2clust = {pred:label for pred,label in zip(pred_types,pred_labels)}
+something = {i:[pred,label] for i,(pred,label) in enumerate(zip(pred_types,pred_labels))}
+pred_clust_df = pd.DataFrame.from_dict(something, orient='index',columns=['pred','cluster'])
+clust2pred = {label:pred_clust_df[pred_clust_df.cluster==label].iloc[0].pred for label in pred_labels}
 
 # -1 stands for "unclustered" in many clustering packages
 # we just convert it to a string
 clust2np[-1] = '-1'
 clust2pred[-1] = '-1'
 
-
-# We define it here because it uses the clusters we just found
+# We define it here rather than above because it uses the clusters we just found.
+# It is just a convenience.
 def clustered_event_dict(event):
     """Based on the found clusters, this function returns a clustered representation
     of an event to a readable one, where each cluster index is replaced by
@@ -270,7 +266,8 @@ def clustered_event_dict(event):
     return event
 
 
-# Collapse each NP into its cluster ID to compress the dataset
+# Collapse each NP into its cluster ID to compress the dataset.
+    # I know it's messy, but it reflects the fact that ChatGPT's output has artifacts in it
 collapsed_db = []
 non_collapsed_db = []
 for entry in evaled_db:
@@ -279,10 +276,10 @@ for entry in evaled_db:
         new_event = {}
 
         # Iterate over the events
-        for role, content in event.items():
+        for  role, content in event.items():
             # Change every pred or NP to their cluster ID
             if role == 'Predicate':
-                new_event[role] = pred2clust.get(content, -1)
+                new_event[role] = pred2clust.get(content,-1)
             else:
                 # The reason we have some cases here is because ChatGPT is not 100% consistent in its output
                 # so I had to just manually account for all cases.
@@ -304,25 +301,21 @@ for entry in evaled_db:
         new_events.append(new_event)
     collapsed_db.append(new_events)
 
-# Collect metadata mapping event indices to the index of their origin document
+# Collect metadata mapping mapping event indices to the index of their origin document
 # We will use it to extract the correct document for each event
 ind2doc = {}
-ind = 0
-for i, events in enumerate(collapsed_db):
+ind=0
+for i,events in enumerate(collapsed_db):
     for _ in events:
-        ind2doc[ind] = i
-        ind += 1
+        ind2doc[ind]=i
+        ind+=1
 
 # This is a flat list of all events, not dividided by docs
 events = flatten(collapsed_db)
 
-# This is just for reference as we want to display the uncollapsed events for comprehensibility
-raw_db = flatten(evaled_db)
-
 # Small event represetnation, just as a tuple of clusters.
 # You can use this as sentence embeddings, for various purpose as we do below.
-small_events = [(event.get('Predicate', None), event.get('Agent', None), event.get('Patient', None),
-                 event.get('Goal', None), event.get('Recipient', None)) for event in events]
+small_events = [(event.get('Predicate',None),event.get('Agent',None),event.get('Patient',None),event.get('Goal',None),event.get('Recipient',None)) for event in events]
 
 # Example usage:
 # Calculate coocurrence probabilities based on the small event representations as vectors
@@ -339,7 +332,8 @@ lowest_k_indices, highest_k_indices = find_top_k_extremes_indices(vector_probs, 
 # The way the prompt is setup now, ChatGPT doesn't tell us which event corresponds to which sentence,
 # so you have to figure it out from the event representation. But this should be pretty straightforward.
 for i in lowest_k_indices:
-    print('Document: ')
+
+    print('Document':)
     print(docs[ind2doc[i]])
     print()
     print('Sentence:')
@@ -351,3 +345,5 @@ for i in lowest_k_indices:
     print('Event with clusters:')
     print(clustered_event_dict(events[i]))
     print('------------------')
+
+
